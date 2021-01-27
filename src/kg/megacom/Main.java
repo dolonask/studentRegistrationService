087b221fb2ee38e2251c9d9d67e2ae01637438b6
ZhyldyzbekOE <osmonkulov.01@gmail.com>
2021-01-28 00:17:01 +0700

    making first commit in branch master

--- src/kg/megacom/Main.java
+++ src/kg/megacom/Main.java
@@ -8,17 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Admin admin = new Admin();
 
-        System.out.println(admin);
-
-        try {
-            Object admin1 = admin.clone();
-
-            System.out.println(admin1);
-        } catch (CloneNotSupportedException e) {
-            e.printStackTrace();
-        }
 
     }
 }
